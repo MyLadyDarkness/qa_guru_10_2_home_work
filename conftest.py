5e12063409935c990042a07a6ba50534f6f74917
Olesya B <OBarkar@it-one.ru>
2024-03-18 15:53:43 +0300

    Fix according to comments

--- conftest.py
+++ conftest.py
@@ -1,12 +1,11 @@
 import pytest
-from selene import browser, be, have
+from selene import browser
 
 
-@pytest.fixture(scope="session")  # all tests will be run in one browser session
+@pytest.fixture(scope="function")
 def browser_settings():
-    browser.driver.set_window_size("800", "600")
-    browser.open('https://www.google.com/')
-    print("Browser fixture")
-    yield  # go to body of test
-    print("Close browser")
-    browser.close()
+    browser.config.base_url = 'https://google.com'
+    browser.config.window_height = 800
+    browser.config.window_width = 600
+    yield
+    browser.quit()
--- test_googlesearch.py
+++ test_googlesearch.py
@@ -1,16 +1,15 @@
-from selene import browser, be, have
-import pytest
+from selene import browser, have
 
 
 def test_search_res(browser_settings):
-    browser.element('[name="q"]').clear()
+    browser.open('')
     browser.element('[name="q"]').type('yashaka/selene').press_enter()
     browser.element('#search').should(
         have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_search_no_res(browser_settings):
-    any_text = "gdsjfgjrwerwerewr"
+    browser.open('')
     browser.element('[name="q"]').clear()
-    browser.element('[name="q"]').type(any_text).press_enter()
+    browser.element('[name="q"]').type("gdsjfgjrwerwerewr").press_enter()
     browser.element('#result-stats').should(have.text('About 0 results'))
